4e01eef0eb9a993bef82d3f456a8c3d00a38c0b0
Jacky Chen <Jacky.Chen-2@ou.edu>
2025-09-10 18:40:32 -0500

    Finished

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name:Jacky Chen
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,34 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+
+            double num1;
+            double num2;
+            double sum;
+            double salesSum;
+            double total;
+            const double salesTax = 8.5;
+            string item;
+            string num1AsString;
+            string num2AsString;
+
+            Console.WriteLine("What Item are you Purchasing");
+            item = Console.ReadLine();
+            Console.WriteLine("How many " + item +"'s would you like to purchase: ");
+            num1AsString = Console.ReadLine();
+            Console.WriteLine("What is the Cost");
+            num2AsString = Console.ReadLine();
+
+           
+            num1 = Convert.ToDouble(num1AsString);
+            num2 = Convert.ToDouble(num2AsString);
+            sum = num1 * num2;
+            salesSum = sum * salesTax;
+            Console.WriteLine("Subtotal: " + sum.ToString("N2"));
+            Console.WriteLine("Sales Tax: " + salesSum.ToString("N2"));
+            total = sum + salesSum;
+            Console.WriteLine("Total: " + total.ToString("N2"));
+            Console.ReadLine();
         }
     }
 }
